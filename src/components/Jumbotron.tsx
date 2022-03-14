175226628f23331c5a0a6c4ec96470e4c3b6a75b
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-03-14 21:32:05 +0100

    feat: import of jumbotron image
    BREAKING CHANGE: #11

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,19 +1,33 @@
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+import profile from "/assets/profil-pic-yasmina.png";
 
 const useStyles = makeStyles(() => ({
  jumbotronUpPart: {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  marginTop: "-80px",
  },
  jumbotronDownPart: {
   display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  alignItems: "center",
  },
  jumbotronTitle: {
   fontFamily: "Playlist",
   fontSize: "100px",
+  marginTop: "-10px",
+  marginBottom: 0,
+ },
+ jumbotronSubtitle: {
+  fontFamily: "Lora",
+  fontSize: "35px",
+  marginTop: 0,
+ },
+ jumbotronCatchPhrase: {
+  fontFamily: "Lora",
+  fontSize: "50px",
  },
 }));
 const Jumbotron: React.FC = () => {
@@ -22,11 +36,16 @@ const Jumbotron: React.FC = () => {
   <div>
    <div className={classes.jumbotronUpPart}>
     <h1 className={classes.jumbotronTitle}>Yasmina Corman</h1>
-    <p>réussir et briller en toute confiance</p>
+    <p className={classes.jumbotronSubtitle}>
+     réussir et briller en toute confiance
+    </p>
    </div>
-   <div className={classes.jumbotronUpPart}>
-    <h2>texte</h2>
-    <p>img</p>
+   <div className={classes.jumbotronDownPart}>
+    <h2 className={classes.jumbotronCatchPhrase}>
+     <b>Amputé(e)</b> de l&apos;expression de ta <b>créativité</b> et de ta
+     singularité? Je t&apos;accompagne à <b>exprimer qui tu es</b> !
+    </h2>
+    <img src={profile} alt="yasmina-corman-image" />
    </div>
   </div>
  );
