cb7f1d7cf9f9ea76760e6a93f100084b7486f272
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-05-23 16:26:19 +0800

    refactor: changed test anchor on cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,13 @@
 Cypress.Commands.add("openCalendlyModal", () => {
- cy.get('[data-test="showCalendlyModal"').click();
- cy.get('[data-test="calendlyModal"').should("exist");
+ cy.get('[dataTest="showCalendlyModal"').click();
+ cy.get('[dataTest="calendlyModal"').should("exist");
 });
 
 Cypress.Commands.add("openNavBurger", () => {
- cy.get('[data-test="navbarBurger"').click();
+ cy.get('[dataTest="navbarBurger"').click();
 });
 
 Cypress.Commands.add("closeCalendlyModal", () => {
- cy.get('[data-test="closeModal"').click();
- cy.get('[data-test="calendlyModal"').should("not.exist");
+ cy.get('[dataTest="closeModal"').click();
+ cy.get('[dataTest="calendlyModal"').should("not.exist");
 });
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -55,7 +55,7 @@ export const Navbar: React.FC<CalendlyHandleModalProps> = (props: {
        //    dataTest="showCalendlyModal"
        className="text-sm p-2 bg-orange text-white font-bold border-2 hover:text-orange hover:bg-white hover:border-orange rounded-3xl"
        onClick={() => props.handleShow()}
-       dataTest="showCalendlyModal"
+       data-test="showCalendlyModal"
       >
        Réserver un entretien gratuit
       </Button>
