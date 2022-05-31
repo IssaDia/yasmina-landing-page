8b2809ccfdcbd79c4ddecd7f7e98dfd88d4cf688
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-05-31 21:45:29 +0800

    feat: added new elements of landing page

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -2,6 +2,43 @@ import React from "react";
 
 export const Nav: React.FC = () => {
  return (
-  <ul className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center flex flex-col items-center justify-center"></ul>
+  <ul className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center flex flex-col items-center justify-center">
+   <div className="flex flex-row space-x-2">
+    <li className="flex flex-row">
+     <a className="text-md uppercase hover:text-orange text-black" href="/">
+      Accueil
+     </a>
+    </li>
+    <li className="flex flex-row">
+     <a
+      className="text-md uppercase hover:text-orange text-black"
+      href="#aboutme"
+     >
+      Qui sommes nous?
+     </a>
+    </li>
+    <li className="flex flex-row">
+     <a
+      className="text-md uppercase hover:text-orange text-black"
+      href="#personalSupport"
+     >
+      Les accompagnements personnalisés
+     </a>
+    </li>
+    <li className="flex flex-row">
+     <a className="text-md uppercase hover:text-orange text-black" href="#mo2i">
+      MO2i
+     </a>
+    </li>
+    <li className="flex flex-row">
+     <a
+      className="text-md uppercase hover:text-orange text-black"
+      href="#reviews"
+     >
+      Témoignages
+     </a>
+    </li>
+   </div>
+  </ul>
  );
 };
--- src/components/Reviews/utils/reviewsInfos.js
+++ src/components/Reviews/utils/reviewsInfos.js
@@ -1,27 +1,27 @@
 export const infos = [
  {
-  name: "Céline Rosiers",
+  name: "Céline R.",
   review:
    "Ce travail personnel est d’un bénéfice inattendu ! Une expérience très enrichissante qui permet d’aboutir à un bien-être personnel et professionnel . Je recommande chaleureusement cette expérience afin de trouver notre « pouvoir » et d’exploiter au mieux notre plein potentiel !",
   imageSrc: "rosiers",
   stars: 5,
  },
  {
-  name: "Claire Bracq",
+  name: "Claire B.",
   review:
    "Yasmina fut une aide précieuse pour me lancer. Au niveau des savoirs théoriques pour commencer (les process à suivre, les aides disponibles etc…), mais son écoute et ses conseils m’ont aussi et surtout permis d’acquérir la confiance qui a rendu possible mon passage à l’acte. Elle continue d’ailleurs à suivre mon évolution et me propose toujours une oreille attentive aux diverses interrogations que je peux encore avoir parfois face aux évolutions du statut d’autoentrepreneur.",
   imageSrc: "bracq",
   stars: 5,
  },
  {
-  name: "Mathilde Rinck",
+  name: "Mathilde R.",
   review:
    "j’ai eu la chance d’avoir Yasmina à mes côtés qui était perspicace, investie à m’aider à tisser mon mode opératoire, à être en accord avec, de façon à l’utiliser plus consciemment dans ma vie de tous les jours, personnelle et professionnelle. ",
   imageSrc: "rinck",
   stars: 5,
  },
  {
-  name: "Selim Slila",
+  name: "Selim S.",
   review:
    " A la suite de l’entretien j’ai ressentis un énorme sentiment de paix et de plénitude et de tout cela avec un regain de motivation et confiance en ma personne et mes projets. J’ai mis des mots sur mon mode opératoire et je suis sûr qu’il me sera d’une grande utilité.",
   imageSrc: null,
